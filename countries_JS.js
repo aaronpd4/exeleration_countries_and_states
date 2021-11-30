8410025ad36cdf638e730c788becac1b68ca2909
Aaron DiFilippo <aaronpd4@vt.edu>
2021-11-30 14:25:09 -0500

    moved function calls to populate dropdowns on page load

--- countries_JS.js
+++ countries_JS.js
@@ -8,6 +8,7 @@ var options = [];
 var stateOptions = [];
 var selectedCountryName = "";
 var prevCountryName = "";
+var created = false;
 
 //use fetch() to send a GET rquest and then it will return a list of objects of the different states.
 //map this GET function to a button, so that when the button is clicked, it will send the request and get the objects,
@@ -24,7 +25,9 @@ function getHttpRequest () {
             countriesList = data;           //make countriesList have all the data from the GET call (countriesList[i] = {id=0, code=AU, name=Austrailia}, etc.)
         }
         showCountries();
+        createOptions();                    //this line HAS to go inside the data arrow function (for some reason?)
     });
+    
 }
 
 function showCountries () {
@@ -43,11 +46,11 @@ function showCountriesStates () {
 function createOptions () {
     if (options.length === 0) {
         for (let i = 0; i < countriesList.length; i++) {
-            //document.createElement("option");          //each index in options is now an option (i.e. option[0] is an option, option[1], etc.)
             options[i] = new Option(`${countriesList[i].name}`, `${countriesList[i].code}`);      //countriesList[i] to get the current country object, then get the name, [2], and the code, [1]
             initCountries.appendChild(options[i], undefined);           //now append the children onto the form (look at bookmarked page)
         }
     }
+    selectedOption();           //adding this line here gets rid of the need for a separate html button
 }
 
 function selectedOption () {
@@ -59,17 +62,7 @@ function selectedOption () {
     var selectedCountryValue = countriesDropdown.options[countriesDropdown.selectedIndex].value;
     console.log(selectedCountryName);
     listStatesForSelCountry(selectedCountryValue);
-
-    //now need to work on adding the options into a list, and also check for duplicates
-    //once in list, work on adding them to the new GET api thing
-
-    /* for (var index = 0; index< options.length; index++) {
-        if(selectedCountry == options[index].name) {
-           //populate new dropdown with selected country 
-           console.log(selectedCountry);
-           break;
-        }
-    } */
+    
 }
 
 function listStatesForSelCountry (countryCode) {
@@ -79,30 +72,47 @@ function listStatesForSelCountry (countryCode) {
     .then(data => {
         countryStatesList = data;
         showCountriesStates();
-    });        
+        createStatesDropdown();
+    });
 }
 
 function createStatesDropdown () {
+        //dynamically create an dropdown list
+        /* if (!created) {
+            //dynamically create the DDL
+            var select = document.createElement("select");
+            select.name = "initStates";
+            select.id = "initStates";
+            for (var k = 0; k < countryStatesList.length; k++) {
+                var option = document.createElement("option");
+                option.value = countryStatesList[k].code;
+                option.text = countryStatesList[k].name;
+                select.appendChild(option);
+            }
+        }
+        created = true; */
+        
         //remove all prev dropdown options
-        //REMOVE IS THE PROBLEM - it is not removing the prev. DDL options
         var x = document.getElementById("initStates");
         var size = (x.options.length - 1);
-        if (selectedCountryName != prevCountryName) {
+        //if (selectedCountryName != prevCountryName) {
             for (let j = size; j >= 0; j--) {
                 x.remove(j);
             }
 
-        //clearing the stateOptions s.t. we can assign new options to the dropdown list
+            //clearing the stateOptions s.t. we can assign new options to the dropdown list
             stateOptions = [];
 
             for (let i = 0; i < countryStatesList.length; i++) {
-                stateOptions[i] = new Option(`${countryStatesList[i].name}`, `${countryStatesList[i].code}`);      //countriesList[i] to get the current country object, then get the name, [2], and the code, [1]
-                initStates.appendChild(stateOptions[i], undefined);           //now append the children onto the form (look at bookmarked page)
+                stateOptions[i] = new Option(`${countryStatesList[i].name}`, `${countryStatesList[i].code}`);      
+                initStates.appendChild(stateOptions[i], undefined);
             }
-        }
+        //}
 
         //resetting the values s.t. there are no duplicates and the country can change
         countryStatesList = [];
         prevCountryName = selectedCountryName;
-        
 }
+
+
+//TODO: sort the countries and states by alpha, create a method to add new countries and then push them
