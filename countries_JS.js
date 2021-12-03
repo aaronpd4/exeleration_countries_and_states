92250cfbfcc75c69c0d47b611ee7e1a2c2ab5624
Aaron DiFilippo <aaronpd4@vt.edu>
2021-12-03 10:57:29 -0500

    Changed functions to fit the new dropdown vs text

--- countries_JS.js
+++ countries_JS.js
@@ -21,7 +21,9 @@ var created = false;
 //and then log the names of each object in the list. returns in form of: object[0].name = austrailia, object[1].name = canada, etc.
 
 
-//"old way of doing it - use fetch instead"
+/**
+ * 
+ */
 function getHttpRequest () {
     url = new Request(countriesURL);
     fetch(url)
@@ -35,16 +37,25 @@ function getHttpRequest () {
         countriesList.sort((a, b) => a.name.localeCompare(b.name));     //this line sorts the countries by name using "a.localeCompare(b)"
         showCountries();
         createOptions();                                                //this line HAS to go inside the data arrow function (for some reason?)
+        addStateDropdown();
     });
     
 }
 
+
+/**
+ * 
+ */
 function showCountries () {
     for (let i=0; i < countriesList.length; i++) {
         console.log(countriesList[i]);
     }
 }
 
+
+/**
+ * 
+ */
 function showCountriesStates () {
     for (let i=0; i < countryStatesList.length; i++) {
         console.log(countryStatesList[i]);
@@ -52,16 +63,25 @@ function showCountriesStates () {
 }
 
 
+/**
+ * 
+ */
 function createOptions () {
+    let x = document.getElementById("initCountries");
+
     if (options.length === 0) {
         for (let i = 0; i < countriesList.length; i++) {
             options[i] = new Option(`${countriesList[i].name}`, `${countriesList[i].code}`);      //countriesList[i] to get the current country object, then get the name, [2], and the code, [1]
-            initCountries.appendChild(options[i], undefined);           //now append the children onto the form (look at bookmarked page)
+            x.appendChild(options[i], undefined);           //now append the children onto the form (look at bookmarked page)
         }
     }
     selectedOption();           //adding this line here gets rid of the need for a separate html button
 }
 
+
+/**
+ * 
+ */
 function selectedOption () {
     var countriesDropdown = document.getElementById("initCountries");
     
@@ -74,6 +94,10 @@ function selectedOption () {
     
 }
 
+
+/**
+ * 
+ */
 function listStatesForSelCountry (countryCode) {
     url = new Request(`https://xc-countries-api.herokuapp.com/api/countries/${countryCode}/states/`);
     fetch(url)
@@ -86,6 +110,10 @@ function listStatesForSelCountry (countryCode) {
     });
 }
 
+
+/**
+ * 
+ */
 function createStatesDropdown () {
         var x = document.getElementById("initStates");
         var size = (x.options.length - 1);
@@ -107,6 +135,10 @@ function createStatesDropdown () {
         prevCountryName = selectedCountryName;
 }
 
+
+/**
+ * 
+ */
 function addCountryUsingHttpPushRequest() {
     var newCountry = document.getElementById("newCountryName").value;       //gets the input from the textbox (name)
     var newCountryCode = document.getElementById("newCountryCode").value;   //"                             " (code)
@@ -118,7 +150,29 @@ function addCountryUsingHttpPushRequest() {
     let jsonInputStringCountry = {"code": `${newCountryCode}`, "name": `${newCountry}`};
     console.log(jsonInputStringCountry);
 
-    $.ajax({
+    fetch(countriesURL, {
+        // Adding method type
+        method: "POST",
+        // Adding body or contents to send
+        body: JSON.stringify({
+            code: `${newCountryCode}`,
+            name: `${newCountry}`
+        }),
+        // Adding headers to the request
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    });
+    document.getElementById("submitNewCountry").submit();
+
+    // Converting to JSON
+    //.then(response => response.json())
+ 
+    // Displaying results to console
+    //.then(json => console.log(json));
+
+    //old ajax version (bad because have to include a whole library when you only use one functionailty of it)
+    /* $.ajax({
        type: "POST",
        url: countriesURL,
        data: JSON.stringify({"code" : `${newCountryCode}`, "name": `${newCountry}`}),
@@ -129,24 +183,37 @@ function addCountryUsingHttpPushRequest() {
        error: function (result, status) {
          console.log(result);
        }
-    });
-
-     document.getElementById("submitNewCountry").submit();
+    }); */
 }
 
 
-//TODO: Create the functionality that adds states to
-//similar to the country addition, create THREE text inputs to choose the code of the new state, the name, and then the country ID it belongs to
+/**
+ * Helper function for the function below it (addStateUsingHttpPushRequest()). 
+ * Creates new options for the drop down when adding a new state.
+ * Called in the initial getHttpRequest() on initial page load
+ */
+function addStateDropdown() {
+    let y = document.getElementById("newStateCountry");
 
+    for (let i = 0; i < countriesList.length; i++) {
+        options[i] = new Option(`${countriesList[i].name}`, `${countriesList[i].code}`);      //countriesList[i] to get the current country object, then get the name, [2], and the code, [1]
+        y.appendChild(options[i], undefined);           //now append the children onto the form (look at bookmarked page)
+    }
+}
+
+
+/**
+ * 
+ */
 function addStateUsingHttpPushRequest() {
     var newStateName = document.getElementById("newStateName").value;
     var newStateCode = document.getElementById("newStateCode").value;
-    var newStateCountry = document.getElementById("newStateCountry").value;
+    var newStateCountry = document.getElementById("newStateCountry").value; //value gets the country code, not id
     var newStateCountryId;
 
     //loop through the countries list to see if the country the state to be added to exists
     for (let i = 0; i < countriesList.length; i++) {
-        if (newStateCountry == countriesList[i].name) {
+        if (newStateCountry == countriesList[i].code) {     //changed condition to check for matching country codes as opposed to names
             newStateCountryId = countriesList[i].id;
             break;
         }
@@ -157,7 +224,24 @@ function addStateUsingHttpPushRequest() {
         let jsonInputStringState = {"code": `${newStateCode}`, "name": `${newStateName}`, "countryId": `${newStateCountryId}`};
         console.log(jsonInputStringState);
 
-        $.ajax({
+        fetch(statesURL, {
+            // Adding method type
+            method: "POST",
+            // Adding body or contents to send
+            body: JSON.stringify({
+                code: `${newStateCode}`,
+                name: `${newStateName}`,
+                countryId: `${newStateCountryId}`
+            }),
+            // Adding headers to the request
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        });
+        document.getElementById("submitNewState").submit();
+
+        //old ajax version (bad because have to include a whole library when you only use one functionailty of it)
+        /* $.ajax({
             type: "POST",
             url: statesURL,
             data: JSON.stringify({"code": `${newStateCode}`, "name": `${newStateName}`, "countryId": `${newStateCountryId}`}),
@@ -168,9 +252,7 @@ function addStateUsingHttpPushRequest() {
             error: function (result, status) {
                 console.log(result);
             }
-            });
-
-        document.getElementById("submitNewState").submit();
+            }); */
     }
     else {
         window.alert("Country does not exist; please enter a valid country!");
